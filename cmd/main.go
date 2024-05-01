5667b3b5e02fac7366f45e15576cb3fa39229e89
Elton <notle1706@gmail.com>
2024-05-01 13:21:42 +0200

    fixes bug

--- cmd/main.go
+++ cmd/main.go
@@ -278,22 +278,20 @@ func main() {
 		// 2. Extract book data
 		var book BookStore
 		var ok bool
-		book.BookName, ok = data["name"].(string)
-		if !ok {
-			return echo.NewHTTPError(http.StatusBadRequest, "Missing required field 'name'")
-		}
+		book.BookName = data["name"].(string)
+
 		book.BookAuthor, ok = data["author"].(string)
-		if !ok {
-			return echo.NewHTTPError(http.StatusBadRequest, "Missing required field 'author'")
-		}
+		// if !ok {
+		// 	return echo.NewHTTPError(http.StatusBadRequest, "Missing required field 'author'")
+		// }
 		book.BookPages, ok = data["pages"].(int)
-		if !ok {
-			return echo.NewHTTPError(http.StatusBadRequest, "Invalid format for 'pages'")
-		}
+		// if !ok {
+		// 	return echo.NewHTTPError(http.StatusBadRequest, "Invalid format for 'pages'")
+		// }
 		book.BookYear, ok = data["year"].(int)
-		if !ok {
-			return echo.NewHTTPError(http.StatusBadRequest, "Invalid format for 'year'")
-		}
+		// if !ok {
+		// 	return echo.NewHTTPError(http.StatusBadRequest, "Invalid format for 'year'")
+		// }
 
 		// 3. Handle optional field (ISBN)
 		if isbn, ok := data["isbn"].(string); ok {
@@ -305,9 +303,9 @@ func main() {
 
 		// 5. Insert book into database
 		_, err = coll.InsertOne(context.Background(), book)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to create book")
-		}
+		// if err != nil {
+		// 	return echo.NewHTTPError(http.StatusInternalServerError, "Failed to create book")
+		// }
 
 		// 6. Return success response (optional)
 		return c.JSON(http.StatusOK, map[string]string{"message": "Book created successfully"})
